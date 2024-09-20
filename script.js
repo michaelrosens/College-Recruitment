e589ddaa5edcd8a69f372368fc17aa1c41ad3c0d
MICHAEL_ROSENSTRAUS <rrosenstrausmichael@gmail.com>
2024-09-20 10:28:51 -0400

    working

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
-const calculator = document.getElementById('tuition-calculator');
+const calculator = document.getElementById('calculator-form');
 calculator.addEventListener('submit', calculateTuition);
 
-function calculateTuition(event){ 
+function calculateTuition(event) {
     event.preventDefault();
+   
+    const credits = parseFloat(document.getElementById('credits').value);
+    const residency = document.getElementById('residency').value;
+   
+    const boarding = parseFloat(document.getElementById('boarding').value);
+    const mealPlan = parseFloat(document.getElementById('meal-plan').value);
+    
+    const books = parseFloat(document.getElementById('books').value);
+    const personal = parseFloat(document.getElementById('personal').value);
+    const transportation = parseFloat(document.getElementById('transp').value);
 
+    let tuition = 0;
+   
+    if (residency === 'in-state') {
+        tuition = 19640;
+    } else {
+        tuition = 25760; 
+    }
+   
+    const totalCost = tuition + boarding + mealPlan + books + personal + transportation;
 
-    let residency = document.getElementById('residency').value;
-    let boarding = parseFloat(document.getElementById('boarding').value);
-    let mealPlan = parseFloat(document.getElementById('meal-plan').value);
-    let credits = 36; 
+    document.getElementById('tuition-result').textContent = 'Estimated Total Cost: $' + totalCost.toFixed(2) + ' per year';
 }
-
-let tuition = 0;
-
-if (residency === 'in-state'){
-    tuition = credits * 300;
-} else {
-    tuition = credits * 600;
-}
-
-const totalCost = tuition + boarding + mealPlan;
-
-
-display.getElementById('tuition-result').textContent = 'Estimated Total Cost: $' + totalCost.toFixed(2) + ' per year';
